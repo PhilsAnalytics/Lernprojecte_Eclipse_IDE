43f3df51a68dca87eecce68a579f74c79a7d30f9
Phil <bergmann.philipp92@gmail.com>
2026-05-28 12:25:34 +0200

    Add Escape.java to 01_Basics folder

---
+++ 01_Basics/Escape/Escape.java
@@ -0,0 +1,33 @@
+
+public class Escape {
+
+	public static void main(String[] args) {
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("> Fehler!!!");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		// Zeilenumbruch mit der Escape-Sequenz \n
+		
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		System.out.println("\n > Fehler!!!");
+		System.out.println();
+		System.out.println();
+		System.out.println();
+		
+		// \n für neue Zeilen, \t für Tabulatoren und \" für Anführungszeichen im Text
+		
+		
+		System.out.println("\n\n\n > Fehler!!!\n\n\n");
+		System.out.println("Er sagte:\n\t Hallo Leute!");
+		System.out.println("Er sagte:\n\"Bye!\n Have a great time!\"");
+		
+
+	}
+
+}
